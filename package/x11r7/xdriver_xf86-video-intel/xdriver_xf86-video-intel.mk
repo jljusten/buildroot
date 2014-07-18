d2875f8de73aa3c387645546b1f876a56c6b81af
Ben Widawsky <ben@bwidawsk.net>
2014-07-17 22:01:16 -0700

    xf86-video-intel: Bump version to 2.99.912
    Warning: I've only built this. Not run it.
    
    Cc: Bernd Kuhls <berndkuhls@hotmail.com>
    Cc: Chris Wilson <chris@chris-wilson.co.uk>
    Signed-off-by: Ben Widawsky <ben@bwidawsk.net>

--- package/x11r7/xdriver_xf86-video-intel/xdriver_xf86-video-intel.mk
+++ package/x11r7/xdriver_xf86-video-intel/xdriver_xf86-video-intel.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XDRIVER_XF86_VIDEO_INTEL_VERSION = 2.99.907
+XDRIVER_XF86_VIDEO_INTEL_VERSION = 2.99.912
 XDRIVER_XF86_VIDEO_INTEL_SOURCE = xf86-video-intel-$(XDRIVER_XF86_VIDEO_INTEL_VERSION).tar.bz2
 XDRIVER_XF86_VIDEO_INTEL_SITE = http://xorg.freedesktop.org/releases/individual/driver
 XDRIVER_XF86_VIDEO_INTEL_LICENSE = MIT
